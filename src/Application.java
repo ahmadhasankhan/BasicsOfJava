2e1a65066c48ca47aaeef90c5b18e3c168eab941
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2015-06-15 19:45:54 +0530

    Made more robust by adding invokeLater SwingUtilities

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,21 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
-		//Initialize new frame
-		JFrame frame = new JFrame("Hello World");		
-		//Close the server on window close
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//Setting window size 
-		frame.setSize(600, 500);				
-		//Making frame visible 
-		frame.setVisible(true);
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run() {
+				//Initialize new frame
+				JFrame frame = new JFrame("Hello World");		
+				//Close the server on window close
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				//Setting window size 
+				frame.setSize(600, 500);				
+				//Making frame visible 
+				frame.setVisible(true);
+			}
+		});
 	}
 }
