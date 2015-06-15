3b9296fe76e66239e9d627764e1df77cdf25ef18
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2015-06-15 19:48:37 +0530

    Hello World

--- src/Application.java
+++ src/Application.java
@@ -1,21 +1,9 @@
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 public class Application {
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run() {
-				//Initialize new frame
-				JFrame frame = new JFrame("Hello World");		
-				//Close the server on window close
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				//Setting window size 
-				frame.setSize(600, 500);				
-				//Making frame visible 
-				frame.setVisible(true);
-			}
-		});
+		System.out.println("Hello Word");
+
 	}
+
 }
