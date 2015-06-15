60567a9b55287ab37181a93785d969a2e4cc479d
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2015-06-15 19:12:24 +0530

    Applied some more Frame properties

--- src/Application.java
+++ src/Application.java
@@ -3,8 +3,14 @@ import javax.swing.JFrame;
 public class Application {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Hello World");
+		
+		//Initialize new frame
+		JFrame frame = new JFrame("Hello World");		
+		//Close the server on window close
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Setting window size 
+		frame.setSize(600, 500);				
+		//Making frame visible 
 		frame.setVisible(true);
 	}
 }
