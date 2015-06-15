26596deaa7257ee41a6beee7810ee74306b367b4
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2015-06-15 18:43:25 +0530

    first commit

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Application {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello World");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
